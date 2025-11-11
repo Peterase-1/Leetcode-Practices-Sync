07c1312179be5ac1da3966e4f92816d893c8205b
Petros Asegid <pasegid@gmail.com>
2025-11-12 01:15:36 +0300

    Time: 1836 ms (43.02%) | Memory: 12.7 MB (25.00%) - LeetSync

---
+++ 474-ones-and-zeroes/ones-and-zeroes.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        """
+        :type strs: List[str]
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        # dp[i][j] = max number of strings with at most i zeros and j ones
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for s in strs:
+            zeros = s.count('0')
+            ones = s.count('1')
+
+            # iterate backwards to prevent reuse of the same string
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    dp[i][j] = max(dp[i][j], 1 + dp[i - zeros][j - ones])
+
+        return dp[m][n]
