dedf52c8bc091e9b5cbddb9612ed3e95b3d9198c
Petros Asegid <pasegid@gmail.com>
2026-02-05 19:34:50 +0300

    Time: 0 ms (100.00%) | Memory: 19.9 MB (40.49%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        # result = []
+        # for i in range ((len(s)-1),0,-1):
+        #     result.append(s[i])
+        return s.reverse()
