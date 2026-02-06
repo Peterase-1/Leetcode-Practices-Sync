09f698e0d972ff61722badb49803482300842363
Petros Asegid <pasegid@gmail.com>
2026-02-06 22:28:47 +0300

    Time: 33 ms (7.25%) | Memory: 12.6 MB (83.53%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        if Counter(s)!=Counter(t):
+            return False
+        if all(letter in s for letter in t):
+            return True
+        return False
