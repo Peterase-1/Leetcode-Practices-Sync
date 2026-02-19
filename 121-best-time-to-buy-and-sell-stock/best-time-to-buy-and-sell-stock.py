37c876ebba516da93842d141daa31436258a41ca
Petros Asegid <pasegid@gmail.com>
2026-02-19 23:19:31 +0300

    Time: 57 ms (71.31%) | Memory: 19.4 MB (35.87%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        min_price = prices[0]
+        max_profit = 0
+
+        for i in range(1, len(prices)):
+            profit = prices[i] - min_price
+            
+            if profit > max_profit:
+                max_profit = profit
+            
+            if prices[i] < min_price:
+                min_price = prices[i]
+        
+        return max_profit
