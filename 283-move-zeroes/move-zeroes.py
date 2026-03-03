b6879c54a97393c7f6ff7582f77c0cb1847d0ec6
Petros Asegid <pasegid@gmail.com>
2026-03-03 22:05:41 +0300

    Time: 0 ms (100.00%) | Memory: 13.6 MB (45.30%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,11 +1,11 @@
 class Solution(object):
-    def moveZeroes(self, nums):
-        pos = 0
+    def moveZeroes(self, arr):
+        sl = 0
 
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[pos] = nums[i]
-                pos += 1
+        for ft in range(len(arr)):
+            if arr[ft] !=0:
+                arr[sl] = arr[ft]
+                sl+=1
 
-        for i in range(pos, len(nums)):
-            nums[i] = 0
+        for i in range(sl,len(arr)):
+            arr[i]=0
