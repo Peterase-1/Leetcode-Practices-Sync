b0ec08573ace386d677f24daf5ab3f5d4f2990e2
Petros Asegid <pasegid@gmail.com>
2026-03-07 23:00:30 +0300

    Time: 0 ms (100.00%) | Memory: 13.1 MB (65.06%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,11 +5,10 @@ class Solution(object):
         for i, num in enumerate(nums):
             complement = target - num
             
-            # Check if complement exists in hash map
             if complement in num_map:
                 return [num_map[complement], i]
             
-            # Store current number and its index
+
             num_map[num] = i
         
-        return []  # No solution found (though problem guarantees one exists)
+        return []
