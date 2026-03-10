61ff2b5272762d040d370d0037e6d5cd3833c767
Petros Asegid <pasegid@gmail.com>
2026-03-10 18:17:24 +0300

    Time: 0 ms (100.00%) | Memory: 13.1 MB (68.18%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        nums = list(set(nums))
+        
+        nums.sort()
+        
+        if len(nums) >= 3:
+            return nums[-3]
+        else:
+            return nums[-1]
