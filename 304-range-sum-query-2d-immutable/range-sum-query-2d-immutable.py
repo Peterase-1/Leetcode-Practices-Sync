61b5f3ce8b6b39028629f72fe8bad579d46b1659
Petros Asegid <pasegid@gmail.com>
2026-03-22 19:19:18 +0300

    Time: 196 ms (39.21%) | Memory: 24.7 MB (53.60%) - LeetSync

---
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -0,0 +1,29 @@
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        if not matrix or not matrix[0]:
+            self.st = []
+            return
+        
+        r = len(matrix)
+        c = len(matrix[0])
+        
+        # st = prefix sum table
+        self.st = [[0] * (c + 1) for _ in range(r + 1)]
+        
+        for i in range(r):
+            for j in range(c):
+                self.st[i+1][j+1] = (
+                    matrix[i][j]
+                    + self.st[i][j+1]
+                    + self.st[i+1][j]
+                    - self.st[i][j]
+                )
+
+    def sumRegion(self, r1, c1, r2, c2):
+        return (
+            self.st[r2+1][c2+1]
+            - self.st[r1][c2+1]
+            - self.st[r2+1][c1]
+            + self.st[r1][c1]
+        )
