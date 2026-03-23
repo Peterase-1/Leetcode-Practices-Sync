2339fd8949839bb7a668de1ec26fb6b802ff784e
Petros Asegid <pasegid@gmail.com>
2026-03-23 13:05:54 +0300

    Time: 48 ms (80.83%) | Memory: 31.5 MB (44.72%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        remainder_map = {0: -1}
+        
+        current_sum = 0
+        
+        for i in range(len(nums)):
+            current_sum += nums[i]
+            
+            remainder = current_sum % k
+            
+            if remainder in remainder_map:
+                if i - remainder_map[remainder] >= 2:
+                    return True
+            else:
+                remainder_map[remainder] = i
+        
+        return False
