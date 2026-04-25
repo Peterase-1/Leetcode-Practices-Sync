1eb888209e4d68203383006ae3b34985501cd13e
Petros Asegid Belayneh <pasegid@gmail.com>
2026-04-25 10:14:11 +0300

    Time: 12 ms (74.92%) | Memory: 12.5 MB (17.90%) - LeetSync

---
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def guessNumber(self, n):
+        low = 1
+        high = n
+        
+        while True:
+            mid = (low + high) // 2
+            
+            result = guess(mid)       
+            if result == 0:
+                return mid        
+            
+            elif result == -1:
+                high = mid - 1
+            
+            else:
+                low = mid + 1
