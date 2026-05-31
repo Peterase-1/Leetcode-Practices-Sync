2bbbac7cdd13a5afd394ebac2326d58341517a13
Petros Asegid <pasegid@gmail.com>
2026-05-31 22:39:57 +0300

    Time: 4 ms (5.54%) | Memory: 12.4 MB (93.97%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class Solution(object):
+    def isSymmetric(self, root):
+        queue = deque([(root.left, root.right)])
+
+        while queue:
+            left, right = queue.popleft()
+
+            if not left and not right:
+                continue
+
+            if not left or not right:
+                return False
+
+            if left.val != right.val:
+                return False
+
+            queue.append((left.left, right.right))
+            queue.append((left.right, right.left))
+
+        return True
